fe876c88fb77b9b59ba569b3b18f47581ee5f890
PataVaucher <andreinapata@gmail.com>
2023-08-29 10:12:47 -0300

    BOTON

--- FRM_PRODUCTOS.Designer.vb
+++ FRM_PRODUCTOS.Designer.vb
@@ -22,16 +22,29 @@ Partial Class FRM_PRODUCTOS
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(274, 127)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(208, 109)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "BOTOOON"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'FRM_PRODUCTOS
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(937, 494)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "FRM_PRODUCTOS"
         Me.Text = "FRM_PRODUCTOS"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents Button1 As Button
 End Class
